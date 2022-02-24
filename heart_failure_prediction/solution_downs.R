65cb931aaf1b14cd6c5989e23e4ad0b09b646400
Jon downs <jon.downs88.jd@gmail.com>
2022-02-24 11:55:36 -0800

    Pushing code so far

--- heart_failure_prediction/solution_downs.R
+++ heart_failure_prediction/solution_downs.R
@@ -5,16 +5,20 @@ library(tidyverse)
 infile <- './heart_failure_prediction/heart_failure_clinical_records_dataset.csv'
 dat <- data.table::fread(infile)
 
-# DATA EXPLORATION --------------------------------------------------------
-# DO NOT SKIP THIS PART :)
+# Make training and testing sets ------------------------------------------
+# Save 20% for testing, 80% for model fit
+train_frac <- round(0.8*nrow(dat))
+train <- sample_n(dat, train_frac)
+test <- dat %>%
+  anti_join(train)
 
 # FIT MODEL ---------------------------------------------------------------
 # Fit a logistic model and produce predicted probabilities for each row
-mod <- glm(DEATH_EVENT ~ ., data = dat, family = 'binomial')
+mod <- glm(DEATH_EVENT ~ ., data = train, family = 'binomial')
 
 # Add predicted values to the data set, assume any modeled prob >0.5 is a yes,
 # and set an indicator variable for whether the model result was accurate
-dat_out <- dat %>%
+dat_out <- train %>%
   mutate(pred = round(mod$fitted, 2),
          predyesno = ifelse(pred > 0.5, 1, 0),
          pred_correct = predyesno == DEATH_EVENT)
@@ -37,3 +41,18 @@ ggplot(sums, aes(x = pred2, y = pevents)) +
   geom_abline(slope = 1, color = 'red') +
   xlab('Predicted probability of event') +
   ylab('Percent of records with an event')
+
+# Get accuracy on testing data --------------------------------------------
+test_preds <- test %>% 
+  mutate(pred = predict(mod, ., type = 'response'),
+         pred = round(pred, 2),
+         predyesno = ifelse(pred > 0.5, 1, 0),
+         pred_correct = predyesno == DEATH_EVENT)
+
+# How accurate were we?
+prop.table(table(test_preds$pred_correct))
+with(test_preds, prop.table(table(DEATH_EVENT, pred_correct), 1))
+# ACCURACY - 85%
+# PPV - 60%
+# NPV - 93%
+# So there's probably room to improve the predictions for those who did die
